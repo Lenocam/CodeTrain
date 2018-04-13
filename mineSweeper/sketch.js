6f0b5e167ccee1a49a9e64d119d06bd768a59322
Reed MacConnell <reed.macconnell@gmail.com>
2018-04-13 08:41:13 -0400

    initial commit

---
+++ mineSweeper/sketch.js
@@ -0,0 +1,70 @@
+function make2DArray() {
+  var arr = new Array(cols);
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+var grid;
+var cols = 20;
+var rows = 20;
+var w = 20;
+
+var totalBees = 25;
+
+function setup() {
+  createCanvas(401, 401);
+  cols = floor(width / w);
+  rows = floor(height / w);
+  grid = make2DArray(cols, rows);
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j] = new Cell(i, j, w);
+    }
+  }
+
+  // Pick totalBees spots
+
+  var options = [];
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      options.push([i,j]);
+    }
+  }
+  for (var n = 0; n < totalBees; n++) {
+    var index = floor(random(options.length));
+    var choice = options[index];
+    var i = choice[0];
+    var j = choice[1];
+    //deletes spot so it is no longer an option
+    options.splice(index, 1);
+    grid[i][j].bee = true;
+
+  }
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].countBees();
+    }
+  }
+}
+
+function mousePressed() {
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      if (grid[i][j].contains(mouseX, mouseY)) {
+        grid[i][j].reveal();
+      }
+    }
+  }
+}
+
+function draw() {
+  background(255);
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].show();
+    }
+  }
+}
